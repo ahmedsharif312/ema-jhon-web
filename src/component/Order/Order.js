6d85b11b5361da4a30b113434007e6d202b028f2
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-04-11 23:09:18 +0600

    added all the tax and shipping inforamtions

--- src/component/Order/Order.js
+++ src/component/Order/Order.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Order.css'
 
 const Order = ({ cart }) => {
+    console.log(cart);
+    let price = 0;
+    let shipping = 0;
+    for (let product of cart) {
+        shipping = shipping + product.shipping;
+        price = price + product.price;
+    }
+    const tax = parseFloat((price * 0.1).toFixed(2));
+    const grandTotal = price + shipping + tax;
+
     return (
-        <div>
+        <div className='order'>
             <h4>Order Summary</h4>
             <p>Selected items: {cart.length}</p>
+            <p>Total Price: ${price}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal}</h5>
         </div>
     );
 };
