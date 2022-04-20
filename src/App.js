4d4d531389b3f329b59df71507046aa9079a15d4
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-04-20 15:57:15 +0600

    create react rout works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './component/AboutUs/About';
 import Headder from './component/Headder/Headder';
+import Home from './component/Home/Home';
+import Products from './component/Products/Products';
 import Shop from './component/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Headder></Headder>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product' element={<Products />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='about' element={<About />} />
+      </Routes>
       <Shop></Shop>
     </div>
   );
