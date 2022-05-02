869d4029f6b1b5985aac8eb3a5279ccbf2074632
Ahmed Sharif <ahmedsharif7605276@gmail.com>
2022-05-03 00:42:15 +0600

    _auth_steps clear

--- src/component/Shipping/Shipping.js
+++ src/component/Shipping/Shipping.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase_init';
 
 const Shipping = () => {
+    const [user] = useAuthState(auth);
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [address, setAddress] = useState('');
@@ -9,10 +12,6 @@ const Shipping = () => {
 
     const handleNameBlur = event => {
         setName(event.target.value);
-    }
-
-    const handleEmailBlur = event => {
-        setEmail(event.target.value);
     }
 
     const handleAddressBlur = event => {
@@ -25,6 +24,8 @@ const Shipping = () => {
 
     const handleCreateUser = event => {
         event.preventDefault();
+        const shipping = (email, name, phone, address);
+        console.log(shipping);
     }
 
     return (
@@ -38,15 +39,15 @@ const Shipping = () => {
                     </div>
                     <div className='input-group'>
                         <label htmlFor="email">Email</label>
-                        <input onBlur={handleEmailBlur} type="email" name="email" id="" required />
+                        <input value={user?.email} readOnly type="email" name="email" id="" required />
                     </div>
                     <div className='input-group'>
                         <label htmlFor="adress">Address</label>
                         <input onBlur={handleAddressBlur} type="password" name="password" id="" required />
                     </div>
                     <div className='input-group'>
-                        <label htmlFor="text">Phone Number</label>
-                        <input onBlur={handlePhoneBlur} type="password" name="password" id="" />
+                        <label htmlFor="phone">Phone Number</label>
+                        <input onBlur={handlePhoneBlur} type="password" name="password" id="" required />
                     </div>
                     <p style={{ color: 'red' }}>{error}</p>
                     <input className='form-submit' type="submit" value="Add Shipping" required />
